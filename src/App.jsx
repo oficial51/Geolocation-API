ddacd1f92816b79bdaa9ab7e8de664fb6d131ee6
oficial51 <ooficial51@gmail.com>
2023-09-27 05:38:35 -0400

    updatetIP

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
         const res = await fetch(
           `https://geo.ipify.org/api/v2/country,city?apiKey=${
             import.meta.env.VITE_REACT_APP_API_KEY
-          }&ipAddress=192.212.174.101`
+          }&ipAddress=192.222.174.121`
         );
         const data = await res.json();
         setAddress(data);
